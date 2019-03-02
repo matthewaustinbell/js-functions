b7f8ba242a7949daed2a2dbbf5dd92035abe4ded
Matt Bell <matthewaustinbell@gmail.com>
2019-03-02 11:41:23 -0600

    adding nuggetizer

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ console.log("hi");
 // const lastName2 = 'Ames';
 // console.log(`${firstName2} ${lastName2}`);
 
-const namePrinter = (firstName,lastName) => {
+const namePrinter = (firstName, lastName) => {
     console.log(`${firstName} ${lastName}`);
 };
 
@@ -25,3 +25,16 @@ const namePrinter = (firstName,lastName) => {
 namePrinter('Zoe', "Ames");
 namePrinter('John', "Doe");
 
+namePrinter('Ames', 'Zoe')
+
+namePrinter(1,2)
+
+
+const nuggetizer = (animal) => {
+    return `processed ${animal}`;
+};
+
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('student'));
+console.log(nuggetizer('duck'));
