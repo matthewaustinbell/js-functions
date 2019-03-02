6295d8a2158dea38b7322dee4dd388a11270c19c
Matt Bell <matthewaustinbell@gmail.com>
2019-03-02 11:07:33 -0600

    name printer function

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("hi");
+// zoe ames
+// Britney Spears
+//Sam Smith 
+//John Doe
+
+// const firstName = 'Zoe';
+// const lastName = 'Ames';
+// //console.log(firstName + ' ' + lastName);
+// console.log(`${firstName} monkeybutt ${lastName}`);
+
+// const firstName1 = 'Britney';
+// const lastName1 = 'Spears';
+// console.log(`${firstName1} ${lastName1}`);
+
+// const firstName2 = 'Tori';
+// const lastName2 = 'Ames';
+// console.log(`${firstName2} ${lastName2}`);
+
+const namePrinter = (firstName,lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+
+
+namePrinter('Zoe', "Ames");
+namePrinter('John', "Doe");
+
